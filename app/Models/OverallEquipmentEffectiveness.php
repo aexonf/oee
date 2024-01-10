794c04bb066b9924870a71b03a9bb5353c4e17fb
AvinFajarF <avinfajar6@gmail.com>
2024-01-10 22:07:14 +0700

    feat: setup model

---
+++ app/Models/OverallEquipmentEffectiveness.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OverallEquipmentEffectiveness extends Model
+{
+    use HasFactory;
+
+    protected $table = 'overall_equipment_effectiveness';
+
+    protected $guarded = [];
+}
