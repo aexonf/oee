d023fbdf003e0383075335d9f57a7b06cad49705
AvinFajarF <avinfajar6@gmail.com>
2024-01-11 21:33:25 +0700

    feat: added a feature to delete all quality data

--- app/Http/Controllers/Back/QualityController.php
+++ app/Http/Controllers/Back/QualityController.php
@@ -11,45 +11,51 @@ use Illuminate\Support\Facades\Session;
 class QualityController extends Controller
 {
 
-    public function index($id){
+    public function index($id)
+    {
         return view('pages.availability.index', ["data" => Quality::find($id)]);
     }
 
 
-  /**
- * Menangani permintaan pembuatan data Quality berdasarkan Performance.
- *
- * @param \Illuminate\Http\Request $request
- * @param int $id ID Performance yang terkait dengan data Quality
- * @return \Illuminate\Http\Response
- */
-public function create(Request $request, $id)
-{
-    // Validasi input data
-    $validasi = $request->validate([
-        "reject_setup" => "integer",
-        "reject_rework" => "integer",
-    ]);
+    /**
+     * Menangani permintaan pembuatan data Quality berdasarkan Performance.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id ID Performance yang terkait dengan data Quality
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request, $id)
+    {
+        // Validasi input data
+        $validasi = $request->validate([
+            "reject_setup" => "integer",
+            "reject_rework" => "integer",
+        ]);
 
-    // Mendapatkan objek Performance berdasarkan ID
-    $performance = Performance::find($id);
+        // Mendapatkan objek Performance berdasarkan ID
+        $performance = Performance::find($id);
 
-    $rateOfQualityProduct = ($performance->jumlah_produksi - $validasi["reject_setup"] - $validasi["reject_rework"]) / $performance->jumlah_produksi * 100;
-    $validasi["rate_of_quality_product"] = $rateOfQualityProduct;
+        $rateOfQualityProduct = ($performance->jumlah_produksi - $validasi["reject_setup"] - $validasi["reject_rework"]) / $performance->jumlah_produksi * 100;
+        $validasi["rate_of_quality_product"] = $rateOfQualityProduct;
 
-    // Membuat data Quality dengan menggunakan model dan data validasi
-    $create = Quality::create($validasi);
+        // Membuat data Quality dengan menggunakan model dan data validasi
+        $create = Quality::create($validasi);
 
-    // Memberikan feedback menggunakan flash message
-    if ($create) {
-        Session::flash("success", "Berhasil membuat data quality");
-    } else {
-        Session::flash("error", "Gagal membuat data quality");
+        // Memberikan feedback menggunakan flash message
+        if ($create) {
+            Session::flash("success", "Berhasil membuat data quality");
+        } else {
+            Session::flash("error", "Gagal membuat data quality");
+        }
+
+        // Kembali ke halaman sebelumnya
+        return redirect()->back();
     }
 
-    // Kembali ke halaman sebelumnya
-    return redirect()->back();
-}
-
+    public function removeAll()
+    {
+        Quality::truncate();
+        return redirect()->back();
+    }
 
 }
