49f370b5c541da0deb5f0a5053bcefad1f1c5316
AvinFajarF <avinfajar6@gmail.com>
2024-01-11 08:29:48 +0700

    chore: setup dashboard

---
+++ resources/views/components/elements/sidebar.blade.php
@@ -0,0 +1,43 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="">PERPUS</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="">SMK</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+
+            <li class="{{ Request::is('admin') ? 'active' : '' }}">
+                <a class="nav-link" href="" id="route-admin"><i class="fas fa-home"></i>
+                    <span>Dashboard</span></a>
+            </li>
+
+            {{-- @if (Auth::user()->role == 'admin' || \App\Models\Librarian::where('user_id', auth()->user()->id)->exists()) --}}
+                <li class="{{ Request::is('') ? 'active' : '' }}">
+                    <a class="nav-link" href="" id="route-admin"><i class="fa-solid fa-book"></i>
+                        <span>Buku</span></a>
+                </li>
+            {{-- @endif --}}
+
+            {{-- @if (\App\Models\Librarian::where('user_id', auth()->user()->id)->exists()) --}}
+                <li class="{{ Request::is('') ? 'active' : '' }}">
+                    <a class="nav-link" href="#" id="route-admin">
+                        <i class="fa fa-tasks"></i>
+                        <span>Pinjaman</span>
+                    </a>
+                </li>
+            {{-- @endif --}}
+
+
+            {{-- @if (Auth::user()->role == 'admin') --}}
+                <li class="{{ Request::is('') ? 'active' : '' }}">
+                    <a class="nav-link" href="#" id="route-admin"><i class="fa fa-user-circle"></i>
+                        <span>Penjaga</span></a>
+                </li>
+            {{-- @endif --}}
+        </ul>
+
+    </aside>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+
+Route::get('/admin', function() {
+    return view('pages.index');
+});
