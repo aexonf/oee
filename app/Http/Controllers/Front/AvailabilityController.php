c0603572ba6d7f43d80885b7c3d22dfff37b797c
AvinFajarF <avinfajar6@gmail.com>
2024-01-11 07:11:49 +0700

    feat: setup controller for availability

---
+++ app/Http/Controllers/Front/AvailabilityController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AvailabilityController extends Controller
+{
+
+
+    public function index(){
+        return view('');
+    }
+
+    public function create(Request $request){
+
+        
+
+    }
+
+
+}
