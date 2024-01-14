fe7d2b0ec3108a021fdce46ad1717f012ff6d7c8
AvinFajarF <avinfajar6@gmail.com>
2024-01-14 14:08:17 +0700

    chore: fixed OEE typo

--- resources/views/components/elements/sidebar.blade.php
+++ resources/views/components/elements/sidebar.blade.php
@@ -1,17 +1,17 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="">EEO</a>
+            <a href="">OEE</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="">EEO</a>
+            <a href="">OEE</a>
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
 
             <li class="{{ Request::is('admin') ? 'active' : '' }}">
                 <a class="nav-link" href="/" id="route-admin"><i class="fas fa-home"></i>
-                    <span>EEO</span></a>
+                    <span>OEE</span></a>
             </li>
 
     </aside>
