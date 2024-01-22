26a080fc73ad6b61b016f60648cc497c2d6f196d
AvinFajarF <avinfajar6@gmail.com>
2024-01-22 08:23:21 +0700

    chore: memperbaiki chart

--- app/Http/Controllers/OeeController.php
+++ app/Http/Controllers/OeeController.php
@@ -10,33 +10,38 @@ use Illuminate\Http\Request;
 class OeeController extends Controller
 {
 
-    public function index(){
+    public function index()
+    {
         $data_oee_weeks = [];
 
 
         foreach (CarbonPeriod::create(Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()) as $date) {
 
-        $data_oee_week = OverallEquipmentEffectiveness::where('created_at', $date->toDateString())->get();
+            $data_oee_week = OverallEquipmentEffectiveness::with(["performance", "availability", "quality"])->where('created_at', $date->toDateString())->get();
 
-        array_push($data_oee_weeks, [
-            "data" => $data_oee_week,
-            "date" => $date->toDateString()
-        ]);
+            /*
+                 rumus rata rata hasil semua oee di bagi jumlah oee
+            */
+       
+        $averageOee = $data_oee_week->avg(function ($item) {
+            return ($item->availability->availability_ratio + $item->performance->performance_efficiency + $item->quality->rate_of_quality_product) / $item->count();
+        });
 
+
+            array_push($data_oee_weeks, [
+                "date" => $date->toDateString(),
+                "averageOee" => $averageOee,
+                "data" => $data_oee_week,
+            ]);
         }
 
 
 
         $data_oee_weeks = collect($data_oee_weeks);
-        // dd($data_oee_weeks);
 
         return view("pages.index", [
             "dataOee" => $data_oee_weeks,
             "data" => OverallEquipmentEffectiveness::with(["performance", "quality", "availability"])->get()
         ]);
-
-
     }
-
-
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -69,6 +69,9 @@
         const dataArray = {!! $dataOee !!};
         const data = dataArray.map(item => item)
 
+        /*
+        rumus rata rata hasil semua oee di bagi jumlah oee
+        */
 
         const ctxData = document.getElementById('cart-data');
 
@@ -78,7 +81,7 @@
                 labels: data.map(item => new Date(item.date).toLocaleDateString()),
                 datasets: [{
                     label: 'OEE',
-                    data: data.map(item => item.data.length),
+                    data: data.map(item => item.averageOee),
                     backgroundColor: '#6777ef'
                 }]
             },
