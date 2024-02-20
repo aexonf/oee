d3f12a2b60cbee7e4a08ead5b33a672c4139678e
aexonf <echo.fajarf@gmail.com>
2024-02-20 08:38:49 +0700

    Feat: added feature for delete oee

--- app/Http/Controllers/OeeController.php
+++ app/Http/Controllers/OeeController.php
@@ -2,38 +2,83 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Availability;
 use App\Models\OverallEquipmentEffectiveness;
+use App\Models\Performance;
+use App\Models\Quality;
 use Carbon\Carbon;
 use Carbon\CarbonPeriod;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class OeeController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
         $data_oee_weeks = [];
 
+        $from = $request->query("from");
+        $to = $request->query("to");
 
-        foreach (CarbonPeriod::create(Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()) as $date) {
+        $oeeQuery = OverallEquipmentEffectiveness::with(["performance", "availability", "quality"]);
 
-            $data_oee_week = OverallEquipmentEffectiveness::with(["performance", "availability", "quality"])->where('created_at', $date->toDateString())->get();
-
-
-
-            array_push($data_oee_weeks, [
-                "date" => $date->toDateString(),
-                "data" => $data_oee_week,
-            ]);
+        if ($request->has('from') && $request->has('to')) {
+            $oeeQuery->whereBetween('created_at', [$from, $to]);
         }
 
+        if ($request->has("from") && $request->has("to")) {
+            $data_oee_week = $oeeQuery->whereBetween('created_at', [$from, $to])->get();
+            foreach (CarbonPeriod::create(Carbon::parse($from)->startOfWeek(), Carbon::parse($to)->endOfWeek()) as $date) {
+                array_push($data_oee_weeks, [
+                    "date" => $date->toDateString(),
+                    "data" => $data_oee_week,
+                ]);
+            }
+        } else {
+            foreach (CarbonPeriod::create(Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()) as $date) {
+                $data_oee_week = $oeeQuery->whereDate('created_at', $date->toDateString())->get();
+                array_push($data_oee_weeks, [
+                    "date" => $date->toDateString(),
+                    "data" => $data_oee_week,
+                ]);
+            }
+        }
 
 
         $data_oee_weeks = collect($data_oee_weeks);
 
         return view("pages.index", [
             "dataOee" => $data_oee_weeks,
-            "data" => OverallEquipmentEffectiveness::with(["performance", "quality", "availability"])->get()
+            "data" => $oeeQuery->get()
         ]);
+    }
+
+
+    public function deleteOee($id)
+    {
+        $oee = OverallEquipmentEffectiveness::find($id);
+
+        if ($oee) {
+            $avaibility = Availability::find($oee->availability_id);
+            $performance = Performance::find($oee->performance_id);
+            $quality = Quality::find($oee->quality_id);
+
+            if ($avaibility && $performance && $quality) {
+                $avaibilityDeleted = $avaibility->delete();
+                $performanceDeleted = $performance->delete();
+                $qualityDeleted = $quality->delete();
+
+                if ($avaibilityDeleted && $performanceDeleted && $qualityDeleted) {
+                    Session::flash('success', 'Data berhasil dihapus.');
+                } else {
+                    Session::flash('error', 'Gagal menghapus data. Terjadi kesalahan dalam menghapus.');
+                }
+            } else {
+                Session::flash('error', 'Gagal menghapus data. Salah satu data terkait tidak ditemukan.');
+            }
+        } else {
+            Session::flash('error', 'Gagal menghapus data. Data tidak ditemukan.');
+        }
     }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Admin - Dashboard')
 
 @push('style')
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 @endpush
 
 @section('main')
@@ -24,44 +24,57 @@
                     </div>
                 </div>
                 <div class="card">
-                    <div class="card-header d-flex justify-content-between">
+                    <div class="card-header d-flex justify-content-between align-items-center">
                         <h3>OEE (Overall Equipment Effectiveness)</h3>
-                        <a href="{{ route('availability') }}" class="btn btn-primary btn-lg">
-                            Buat
-                        </a>
+                        <div class="d-flex align-items-center">
+                            <div class="form-group mr-3 mb-0">
+                                <input type="date" class="form-control" id="start_date" name="from">
+                            </div>
+                            <div class="form-group mr-3 mb-0">
+                                <input type="date" class="form-control" id="end_date" name="to">
+                            </div>
+                            <button type="button" class="btn btn-icon icon-left btn-info mr-2 mb-2"
+                            data-toggle="collapse" data-target="#section-filter"><i class="fas fa-filter"></i>
+                            Filter</button>                            <a href="#" class="btn btn-success" id="export_btn">Export</a>
+                            <a href="{{ route('availability') }}" class="btn btn-primary btn-lg ml-3">Buat</a>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <table class="table">
-                            <thead>
+
+
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Availability Ratio</th>
+                                <th scope="col">Performance Efficiency</th>
+                                <th scope="col">Rate Of Quality Product</th>
+                                <th scope="col">Overall Equipment Effectiveness</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($data as $index => $item)
                                 <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Availability Ratio</th>
-                                    <th scope="col">Performance Efficiency</th>
-                                    <th scope="col">Rate Of Quality Product</th>
-                                    <th scope="col">Overall Equipment Effectiveness</th>
-                                    <th scope="col">Action</th>
+                                    <th scope="row">{{ $index + 1 }}</th>
+                                    <td>{{ $item->availability->availability_ratio }}</td>
+                                    <td>{{ $item->performance->performance_efficiency }}</td>
+                                    <td>{{ $item->quality->rate_of_quality_product }}</td>
+                                    <td>{{ ($item->availability->availability_ratio * $item->performance->performance_efficiency * $item->quality->rate_of_quality_product) / 10000 }}
+                                    </td>
+                                    <td><a href="{{ route('availability.detail', $item->availability->id) }}"><i
+                                                class="bi bi-eye-fill"></i></a></td>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($data as $index => $item)
-                                    <tr>
-                                        <th scope="row">{{ $index + 1 }}</th>
-                                        <td>{{ $item->availability->availability_ratio }}</td>
-                                        <td>{{ $item->performance->performance_efficiency }}</td>
-                                        <td>{{ $item->quality->rate_of_quality_product }}</td>
-                                        <td>{{ ($item->availability->availability_ratio * $item->performance->performance_efficiency * $item->quality->rate_of_quality_product) / 10000 }}
-                                        </td>
-                                        <td><a href="{{route('availability.detail', $item->availability->id)}}"><i class="bi bi-eye-fill"></i></a></td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
+    </div>
 
 
-        </section>
+    </section>
     </div>
 @endsection
 
@@ -118,4 +131,25 @@
             document.getElementById('route-admin').click();
         </script>
     @endif
+
+    @push('scripts')
+    <!-- JS Libraries -->
+    <script src="{{ asset('library/datatables/media/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('library/datatables/media/js/dataTables.min.js') }}"></script>
+    <script src="{{ asset('library/jquery-ui-dist/jquery-ui.min.js') }}"></script>
+    <script src="{{ asset('js/page/modules-datatables.js') }}"></script>
+    <script src="{{ asset('library/select2/dist/js/select2.full.min.js') }}"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('.select2').select2();
+        });
+
+        const handleChangeFilter = (e) => {
+            const currentURL = new URL(window.location.href);
+            currentURL.searchParams.set(e.name, e.value);
+            window.history.pushState({}, '', currentURL);
+            location.reload();
+        }
+    </script>
 @endpush
