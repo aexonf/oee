941450d4baf75bfe584304c26caad97a1aa5273b
Aexon <128112182+aexonf@users.noreply.github.com>
2024-02-21 23:10:47 +0700

    Update OeeExport.php

--- app/Exports/OeeExport.php
+++ app/Exports/OeeExport.php
@@ -17,34 +17,28 @@ class OeeExport implements  FromView, ShouldAutoSize
     public function view(): View
     {
         // Mengambil data 1 minggu terakhir
-        $endDate = Carbon::now();
-        $startDate = $endDate->copy()->subWeek();
+    //    $endDate = Carbon::now();
+      //  $startDate = $endDate->copy()->subWeek();
 
         // Mengambil semua data
-        $oeeData = OverallEquipmentEffectiveness::with(["performance", "quality", "availability"])->whereBetween('created_at', [$startDate, $endDate])->get();
+        $oeeData = OverallEquipmentEffectiveness::with(["performance", "quality", "availability"])->get();
 
         // Menghitung jumlah hari dalam rentang waktu data yang diambil
-        $daysInRange = $endDate->diffInDays($startDate) + 1;
+       // $daysInRange = $endDate->diffInDays($startDate) + 1;
 
         // Menghitung rata-rata OEE per hari
-        $totalOee = $oeeData->sum('oee');
-        $averageOeePerDay = $totalOee / $daysInRange;
+      //  $totalOee = $oeeData->sum('oee');
+        //$averageOeePerDay = $totalOee / $daysInRange;
 
         // Mengalikan setiap nilai OEE dengan rasio hari dalam seminggu dan jumlah hari data yang ada
-        $oeeData->transform(function ($item) use ($daysInRange, $averageOeePerDay) {
-            $item->oee = $item->oee * (7 / $daysInRange);
-            return $item;
-        });
+        //$oeeData->transform(function ($item) use ($daysInRange, $averageOeePerDay) {
+          //  $item->oee = $item->oee * (7 / $daysInRange);
+            //return $item;
+        //});
 
         return view('pages.format-export-oee', [
             "data" => $oeeData,
         ]);
-    }
-
-
-    public function export()
-    {
-        return Excel::download(new OeeExport, 'oee.xlsx');
     }
 
 
