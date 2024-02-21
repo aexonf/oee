8c606e0cdc9d77d19e0a2678e7e8f10e7817ef75
Aexon <128112182+aexonf@users.noreply.github.com>
2024-02-21 23:17:29 +0700

    Update format-export-oee.blade.php

--- resources/views/pages/format-export-oee.blade.php
+++ resources/views/pages/format-export-oee.blade.php
@@ -16,7 +16,7 @@
                 <td>{{ $value->availability->availability_ratio }}</td>
                 <td>{{ $value->performance->performance_efficiency }}</td>
                 <td>{{ $value->quality->rate_of_quality_product }}</td>
-                <td>{{ $value->availability->availability_ratio +  $value->performance->performance_efficiency + $value->quality->rate_of_quality_product }}</td>
+<td>{{ ceil($value->availability->availability_ratio + $value->performance->performance_efficiency + $value->quality->rate_of_quality_product) }}</td>
             </tr>
         @endforeach
     </tbody>
